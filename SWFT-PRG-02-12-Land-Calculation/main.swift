2da69fcee48ee6d0d19587cee7ef33c589ff9eb9
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-10-11 06:39:23 -0400

    Initial version commented and working.

--- SWFT-PRG-02-12-Land-Calculation/main.swift
+++ SWFT-PRG-02-12-Land-Calculation/main.swift
@@ -5,8 +5,24 @@
 //  Created by Keith Smith on 10/11/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  One acre of land is equivalent to 43,560 square feet. Write a program that calculates
+//  the number of acres in a tract of land with 391,876 square feet.
 
+// Import for string formatting
 import Foundation
 
-print("Hello, World!")
+// Define constants
+let acre = 43560
+let land = 391876
 
+// Define variable
+var acres: Double
+
+// Cast ints as doubles and calculate acres
+acres = Double(land) / Double(acre)
+
+// Format calculated acres with single decimal precision
+let formattedOutput = String(format: "%.1f", acres)
+
+// Print calculated value to console
+print(land, "square feet is", formattedOutput, "acres")
