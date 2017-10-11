3e70c0476e650d6cc042a29fda41f3117b48b847
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-10-11 06:33:50 -0400

    Initial Commit

---
+++ SWFT-PRG-02-12-Land-Calculation/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SWFT-PRG-02-12-Land-Calculation
+//
+//  Created by Keith Smith on 10/11/17.
+//  Copyright © 2017 Keith Smith. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
